a50562d7f78279bc0c55760ef4a0690f5222194a
Martin Larralde <martin.larralde@embl.de>
2025-05-15 20:23:39 +0200

    Move some project metadata and `cibuildwheel` configuration to `pyproject.toml`

--- setup.py
+++ setup.py
@@ -38,22 +38,6 @@ def _eprint(*args, **kwargs):
 
 # --- `setup.py` commands ----------------------------------------------------
 
-class sdist(_sdist):
-    """A `sdist` that generates a `pyproject.toml` on the fly.
-    """
-
-    def run(self):
-        # build `pyproject.toml` from `setup.cfg`
-        c = configparser.ConfigParser()
-        c.add_section("build-system")
-        c.set("build-system", "requires", str(self.distribution.setup_requires))
-        c.set("build-system", 'build-backend', '"setuptools.build_meta"')
-        with open("pyproject.toml", "w") as pyproject:
-            c.write(pyproject)
-        # run the rest of the packaging
-        _sdist.run(self)
-
-
 class build_ext(_build_ext):
     """A `build_ext` that disables optimizations if compiled in debug mode.
     """
@@ -649,6 +633,5 @@ setuptools.setup(
         build_clib=build_clib,
         clean=clean,
         configure=configure,
-        sdist=sdist,
     ),
 )
